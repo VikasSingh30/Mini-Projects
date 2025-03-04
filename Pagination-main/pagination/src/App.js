6b7ce691291fa11907b1d88bbe037265740c91e7
Vikas <singhvikas.sv30@gmail.com>
2025-03-04 06:35:00 +0000

    all

---
+++ Pagination-main/pagination/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import React from 'react';
+import Pagination from './Pagination';
+
+function App() {
+  return (
+    <div className="App">
+      <Pagination/>
+    </div>
+  );
+}
+
+export default App;
