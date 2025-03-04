822cc0c9ff0b59763e1047aefe515f689fcf71f8
Vikas <singhvikas.sv30@gmail.com>
2025-03-04 06:32:31 +0000

    4 projects

---
+++ Counter-main/counter/src/Counter.js
@@ -0,0 +1,33 @@
+import React from 'react';
+
+class Counter extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { count: 0 };
+  }
+
+  increment = () => {
+    this.setState({ count: this.state.count + 1 });
+  }
+
+  decrement = () => {
+    this.setState({ count: this.state.count - 1 });
+  }
+
+  render() {
+    const counterStyle = {
+      marginLeft: '10px',
+    };
+
+    return (
+      <div style={counterStyle}> 
+        <h1>Counter App</h1>
+        <p>Count: {this.state.count}</p>
+        <button onClick={this.increment}>Increment</button>
+        <button onClick={this.decrement}>Decrement</button>
+      </div>
+    );
+  }
+}
+
+export default Counter;
